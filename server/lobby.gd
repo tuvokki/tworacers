fcdb7fa5c210e904084b7d906a34bc26d934bb30
Wouter Roosendaal <tuvokki@gmail.com>
2023-09-12 19:25:32 +0200

    Game setup networking

--- server/lobby.gd
+++ server/lobby.gd
@@ -1,11 +1,62 @@
 extends Control
 
+# These signals can be connected to by a UI lobby scene or the game scene.
+signal player_connected(peer_id, player_info)
+signal player_disconnected(peer_id)
+signal server_disconnected
+
+const PORT = 7000
+const DEFAULT_SERVER_IP = "127.0.0.1" # IPv4 localhost
+const MAX_CONNECTIONS = 20
+
+# This will contain player info for every player, with the keys being each player's unique IDs.
+var players = {}
+var players_loaded = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func create_game():
+	var peer = ENetMultiplayerPeer.new()
+	var error = peer.create_server(PORT, MAX_CONNECTIONS)
+	if error:
+		return error
+	multiplayer.multiplayer_peer = peer
+
+# When the server decides to start the game from a UI scene, do Lobby.load_game.rpc(filepath)
+@rpc("call_local", "reliable")
+func load_game(game_scene_path):
+	get_tree().change_scene_to_file(game_scene_path)
+
+# Every peer will call this when they have loaded the game scene.
+@rpc("any_peer", "call_local", "reliable")
+func player_loaded():
+	if multiplayer.is_server():
+		players_loaded += 1
+		if players_loaded == players.size():
+			$/root/Game.start_game()
+			players_loaded = 0
+
+# When a peer connects, send them all player info.
+# This allows transfer of all desired data for each player, not only the unique ID.
+func _on_player_connected(id):
+	_register_player.rpc_id(id, players)
+
+@rpc("any_peer", "reliable")
+func _register_player(new_player_info):
+	var new_player_id = multiplayer.get_remote_sender_id()
+	players[new_player_id] = new_player_info
+	player_connected.emit(new_player_id, new_player_info)
+
+
+func _on_connect_button_pressed():
+	var game = create_game()
+	if game == OK:
+		# change to main game scene
+		pass
+	print("Game init failed")
